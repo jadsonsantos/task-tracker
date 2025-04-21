b27b11d43cafe162f62ef7edb652a16c27dbe532
Jadson Santos <jadsondossantos5@gmail.com>
2025-04-21 19:07:09 -0300

    task services

--- src/app/services/task.service.ts
+++ src/app/services/task.service.ts
@@ -1,9 +1,28 @@
 import { Injectable } from '@angular/core';
+import { Task } from '../models/task.model'
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class TaskService {
+  private tasks: Task[] = []
+  private nextId = 1
 
-  constructor() { }
+  getTasks(): Task[] {
+    return this.tasks
+  }
+  addTask(title: string, description: string): void {
+    this.tasks.push({ id: this.nextId++, title, description, done: false })
+  }
+  deleteTask(id: number): void {
+    this.tasks = this.tasks.filter((task) => task.id !== id)
+  }
+  updateTask(task: Task): void {
+    const index = this.tasks.findIndex((t) => t.id === task.id)
+    if (index !== -1) {
+      this.tasks[index] = task
+    }
+  }
+
+  constructor() {}
 }
